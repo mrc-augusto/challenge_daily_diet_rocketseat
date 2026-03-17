5f807986a7292eab4209a2ee7a69cacf6993cffb
Maurício Augusto <mrc_augusto@hotmail.com>
2026-03-17 15:31:03 -0300

    Add initial Flask app setup with database configuration and hello world route

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from flask import Flask, request, jsonify
+from config import DATABASE_URI
+from database import db
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+db.init_app(app)
+
+@app.route('/hello-world', methods=['GET'])
+def hello_world():
+  return 'Hello World!'
 
 if __name__ == '__main__':
   app.run(debug=True)
---
+++ config.py
@@ -0,0 +1,4 @@
+# config.py
+# Configuração da URI do banco de dados
+
+DATABASE_URI = 'sqlite:///dieta.db'
